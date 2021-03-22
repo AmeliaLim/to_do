04386b2ec1937aa7fe8714e7aa928f43b8475927
Amelia <limamelia.yx@gmail.com>
2021-03-22 21:54:05 +1100

    Updating Home.js

--- Routes/home.js
+++ Routes/home.js
@@ -3,17 +3,25 @@ const router = express.Router();
 const db = require('../db/database.js');
 
 router.get("/", (req, res) => {
-    db.any("SELECT*").from("task")
+    db.any('SELECT *  FROM todo => {
       .then((todolist) => {
         res.render("pages/home", {
           todolist: todolist,
           title: "Home | Mach Fuenf einfach"
         });
       })
+      )
       .catch((err) => res.render("pages/error", {
           err: err,
           title: "Error | Mach Fuenf einfach"
       }));
   });
   
+  router.post("/addgoal", (req, res) => {
+    db.none("INSERT INTO )
+    .catch((err) => res.render("pages/error", {
+      err: err,
+      title: "Error | Mach Fuenf einfach"
+  }));
+  }
   module.exports = router;
